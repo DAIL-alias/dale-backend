feb5900aefddb02c7b364f8e5589d10504f79bed
Afam <itsafam@gmail.com>
2024-08-20 01:30:20 +0100

    Created email alias model

---
+++ models/aliases.go
@@ -0,0 +1,17 @@
+package alias
+
+import (
+	"time"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Alias struct {
+	AliasID     int       `gorm:"primaryKey;autoIncrement;column: alias_id;unique;not null" json:"aliasId"`
+	UserID      int       `gorm:"column: user_id;unique;not null"                           json:"userId" `
+	AliasPrefix string    `gorm:"column: alias_prefix;unique;not null"                      json:"aliasPrefix"`
+	IsActive    bool      `gorm:"column: is_active;not null;default: true"                  json:"isActive"`
+	IsDeleted   bool      `gorm:"column: is_deleted;not null;default:false"                 json:"isDeleted"`
+	ExpiresAt   time.Time `gorm:"column: expires_at;not null"                               json:"expiresAt"`
+	CreatedAt   time.Time `gorm:"column: created_at;not null"                               json:"createdAt"`
+}
