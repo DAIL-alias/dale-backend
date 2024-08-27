45a69ee083c886e8abef428dc8bebd823d30e18e
cy <40525455+rsclip@users.noreply.github.com>
2024-08-27 04:54:36 +0100

    Trigger + migrations

--- models/user.go
+++ models/user.go
@@ -8,11 +8,12 @@ import (
 
 type User struct {
 	gorm.Model
-	Email      string `gorm:"column:email;unique;not null"                                 json:"email"     `
-	Password   string `gorm:"column:password;not null"                                     json:"password"  `
-	Salt       string `gorm:"column:salt;not null"                                                          `
-	NumAliases int    `gorm:"column: num_aliases;not null;default:0"                       json:"numAliases"`
-	Role       int    `gorm:"column:role;not null;default:0"                               json:"role"      `
+	Email     		 	string `gorm:"column:email;unique;not null"                                 json:"email"     `
+	Password  			string `gorm:"column:password;not null"                                     json:"password"  `
+	Salt       		    string `gorm:"column:salt;not null"                                                          `
+	Role                int    `gorm:"column:role;not null;default:0"                               json:"role"      `
+	NumAliases          int    `gorm:"column:num_aliases;not null;default:0"                        json:"numAliases"`
+	NumUndeletedAliases int    `gorm:"column:num_undeleted_aliases;not null;default:0"              json:"numUndeletedAliases"`
 }
 
 // Hook to hash password and assign salt to the user
